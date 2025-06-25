c8dd99992d1ae2c1a6e6697db212f7e9efca78c8
J0n4s4n <jonh.reyes.go@gmail.com>
2025-06-25 13:33:24 -0600

    feat: ✨ implement form validation structure and event handling for improved user interaction

--- 04-Form-validation/project/app.js
+++ 04-Form-validation/project/app.js
@@ -4,10 +4,53 @@
 // The goal is to create a form validation system that can be easily extended and configured.
 // Yes, I did deleted the previous code to start fresh with a new approach.
 
-const formValidationConfig = [];
-const formValidation = () => {};
-const formHandler = (event) => {
-  event.preventDefault();
-};
+document.addEventListener("DOMContentLoaded", () => {
+  //TODO: It should be an object array with the following structure: Field, and Rules
+  const formValidationConfig = [
+    {
+      //? It should capture a DOM element directly or call a variable prevuiously defined?
+      field: document.getElementById("name"),
+      rules: {
+        required: true,
+        notEmpty: true,
+        regEx: true,
+      },
+    },
+  ];
 
-document.addEventListener('DOMContentLoaded', () => {});
+  //TODO: It should be a function that validates ONE field based on the rules defined in the formValidationConfig
+  const formFieldValidator = (formField) => {
+    console.log("Field loaded:", formField.field.name);
+  };
+
+  //TODO: It should be a function that handles the form submission but it should be agnostic to the form structure, it should iterate over the form fields and validate them based on the rules defined in the formValidationConfig, using formFieldValidator
+  //! It should work in real time, (on input change) and on form submission
+  const formHandler = (event) => {};
+
+  //TODO: It should be a function that handles the form submission success, it should prevent the default form submission behavior
+  const formHandlerSucces = (event) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+  };
+
+  const inicializeForm = () => {
+    // ? Early exit if the form element is not found
+    if (!document.getElementById("register-form")) {
+      console.error("Form element not found");
+      return;
+    }
+    const form = document.getElementById("register-form");
+    // Iterate over the formValidationConfig to add event listeners to each field
+    for (const field of formValidationConfig) {
+      // Add event listener for input change
+      console.log("Adding event listener to field:", field.field.name);
+      field.field.addEventListener("input", (event) => {
+        formFieldValidator(field);
+      });
+    }
+    // Add event listener for form submission
+    form.addEventListener("submit", formHandlerSucces);
+  };
+
+  inicializeForm();
+});
